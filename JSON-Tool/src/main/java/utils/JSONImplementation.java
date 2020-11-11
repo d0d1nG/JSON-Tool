7e366c4321f6bf92aa2c0967a32dbfb74ee48f9d
DESKTOP-L6O7F6S\bosko <boskovicnikola99@gmail.com>
2020-11-11 16:18:34 +0100

    a

--- JSON-Tool/src/main/java/utils/JSONImplementation.java
+++ JSON-Tool/src/main/java/utils/JSONImplementation.java
@@ -24,9 +24,12 @@ import com.google.gson.stream.JsonToken;
 
 import models.Entity;
 import utils.API;
+import utils.ToolManager;
+import utils.JSONImplementation;
+
 
 public class JSONImplementation extends API {
-	private static int br = 1;
+	public String a;
 	static {
 		ToolManager.registerManager(new JSONImplementation(), ".json");
 	}
@@ -55,11 +58,13 @@ public class JSONImplementation extends API {
 						jr.beginObject();
 						while (jr.hasNext()) {
 							String name1 = jr.nextName();
+							System.out.println(name1 + " !@#!@#!@##!@");
 							if (name1.equals("name")) {
 								nested.setName(jr.nextString());
 							} else if (name1.equals("id")) {
 								nested.setId(jr.nextString());
 							} else {
+								System.out.println("usao");
 								nested.getProperties().put(name1, jr.nextString());
 							}
 						}
@@ -79,10 +84,10 @@ public class JSONImplementation extends API {
 				}
 				entities.add(e);
 				jr.endObject();
-
+				
 			}
 			jr.endArray();
-			System.out.println(entities);
+			jr.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -162,7 +167,8 @@ public class JSONImplementation extends API {
 										nested.getProperties().put(name1, jr.nextString());
 									}
 								}
-								e.getEntities().put(name, nested);
+								if (!nested.getId().equals(id))
+									e.getEntities().put(name, nested);
 								jr.endObject();
 							} else {
 								String value = jr.nextString();
@@ -183,6 +189,7 @@ public class JSONImplementation extends API {
 
 					}
 					jr.endArray();
+					jr.close();
 				}catch(Exception e) {
 					e.printStackTrace();
 				}
@@ -217,9 +224,108 @@ public class JSONImplementation extends API {
 	}
 
 	@Override
-	public void brisiKljucVrednost(List<Entity> data, String key, String value, String dirPath) {
-		// TODO Auto-generated method stub
+	public void brisiKljucVrednost(String name_, String key, String value_, String dirPath) {
+		entities.clear();
+		File directory = new File(dirPath);
+		File[] contents = directory.listFiles();
+		
+		for (File f : contents) {
+			List<Entity> entities1 = new ArrayList<Entity>();
+			if (f.length() > 0) {
+				JsonReader jr;
+				try {
+					jr = new JsonReader(new FileReader(f));
+					jr.beginArray();
+					while (jr.hasNext()) {
+						Entity e = new Entity();
 
+						jr.beginObject();
+						while (jr.hasNext()) {
+
+							String name = jr.nextName();
+
+							if (jr.peek() == JsonToken.BEGIN_OBJECT) {
+								Entity nested = new Entity();
+								jr.beginObject();
+								while (jr.hasNext()) {
+									String name1 = jr.nextName();
+									if (name1.equals("name")) {
+										nested.setName(jr.nextString());
+									} else if (name1.equals("id")) {
+										nested.setId(jr.nextString());
+									} else {
+										String vrednost = jr.nextString();
+										if ((!name1.equals(name_) && !vrednost.equals(value_)) || (name_.isEmpty() || vrednost.isEmpty()))
+											nested.getProperties().put(name1, vrednost);
+									}
+								}
+								if (!name.equals(name_) || name_.isEmpty()) {
+									e.getEntities().put(name, nested);
+								}
+								
+								jr.endObject();
+							} else {
+								String value = jr.nextString();
+								if (name.equals("name")) {
+									e.setName(value);
+								} else if (name.equals("id")) {
+									e.setId(value);
+								} else {
+									e.getProperties().put(name, value);
+								}
+
+							}
+						}
+						if (!e.getName().equals(name_) || name_.isEmpty()) {
+							/// iterirati kroz propertije i proveravati da li se za kljuc nalazi ta vrednost i ako se ne nalazi dodati je
+//							if (!key.isEmpty() && !value_.isEmpty()) {
+//								for (Map.Entry<String,Object> entry : e.getProperties().entrySet()) {  
+//
+//									if (!entry.getKey().equals(key) && !entry.getValue().equals(value_)) {
+//						            	entities1.add(e); /// MISLIM DA CE OVDE DA DUPLIRA
+//						            }
+//									
+//								}
+//							}
+							//else {
+								entities1.add(e);
+						//	}
+						}
+						jr.endObject();
+
+					}
+					jr.endArray();
+					jr.close();
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+				
+				
+				try {
+					FileWriter fileWriter = new FileWriter(new File(f.getAbsolutePath()));
+					JsonArray sArray = new JsonArray();
+					for (Entity e : entities1) {
+						JsonObject json = new JsonObject();
+						json.addProperty("id", e.getId());
+						json.addProperty("name", e.getName());
+						for (Map.Entry<String, Object> property : e.getProperties().entrySet()) {
+							json.addProperty(property.getKey(), (String) property.getValue());
+						}
+						for (Map.Entry<String, Entity> entity : e.getEntities().entrySet()) {
+							json.addProperty(entity.getKey(), entity.getValue().toString());
+						}
+						sArray.add(json);
+					}
+					fileWriter.write(sArray.toString());
+					fileWriter.close();
+					entities.addAll(entities1);
+				} catch (Exception e) {
+
+					e.printStackTrace();
+				}
+				
+			}
+		}
 	}
 
 }
